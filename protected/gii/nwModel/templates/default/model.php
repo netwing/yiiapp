70c244e4494a66d84cc8dcff3bc9daf28410c3a4
Emanuele Deserti <emanuele.deserti@netwing.it>
2013-10-08 12:17:16 +0200

    Method model() in AR class must exists

--- protected/gii/nwModel/templates/default/model.php
+++ protected/gii/nwModel/templates/default/model.php
@@ -17,4 +17,15 @@ Yii::import('<?php echo "{$this->baseModelPath}.{$this->baseModelClass}"; ?>');
 class <?php echo $modelClass; ?> extends <?php echo $this->baseModelClass."\n"; ?>
 {
 
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return <?php echo $modelClass; ?> the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
 }
